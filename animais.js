17413bd863f5c1b4f53cd4c6945e0c5701533c0f
guicvsub <rm552321@fiap.com.br>
2023-10-28 19:18:36 -0300

    checkpoint 02

---
+++ animais.js
@@ -0,0 +1,9 @@
+//declarando variaveis
+let animais = ["gato", "macaco", "pato", "galo","leao"];
+//estrutura de verificação para checar se leao não existe no array 
+if (!animais.includes("leao")) {
+    console.log("leao nao existe no array");
+} else {
+    console.log("leao existe no array");
+}
+def
---
+++ series.js
@@ -0,0 +1,10 @@
+//declarando variáveis
+const listTvShows = ["todo mundo odeia o chris","arnold","the big bang theory"]
+let listaVerificacao = false;
+if (listTvShows.length ===2){
+        listaVerificacao = true;
+        console.log("essa lista possui 3 itens ",listaVerificacao)        
+}
+else {
+        console.log("essa lista possui 3 itens",listaVerificacao)
+}
